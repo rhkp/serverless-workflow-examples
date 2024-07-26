05dd8ef9e0d0140e91cb25165af83318a27f7b68
rhkp <kpanchal@redhat.com>
2024-07-26 10:20:37 -0400

    Proposed impl for Jira integration

---
+++ mta/v7.x/openshift-local-setup/envup-openshift-local.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+CRC_STATUS=$(crc status | grep Running)
+if [ -n "$CRC_STATUS" ]; then
+    echo "OpenShift Local already running "
+    oc login -u kubeadmin https://api.crc.testing:6443
+else
+    echo "OpenShift Local not running "
+    # Cleanup previous instance
+    crc stop
+    crc delete -f
+    CRC_START_CMD_OUTPUT=$(crc start -o json)
+
+    echo "crc start output: $CRC_START_CMD_OUTPUT" 
+
+    # Extract variables
+    ADMIN_USER=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.adminCredentials.username" | tr -d "\"") 
+    ADMIN_PWD=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.adminCredentials.password" | tr -d "\"")
+    API_URL=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.url" | tr -d "\"")
+    WEB_CONSOLE_URL=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.webConsoleUrl" | tr -d "\"")
+    echo "ADMIN_USER: $ADMIN_USER, ADMIN_PWD: $ADMIN_PWD, API_URL: $API_URL, WEB_CONSOLE_URL: $WEB_CONSOLE_URL"
+
+    # Open Web Console
+    crc console
+
+    # Login to the cluster
+    oc login -s=$API_URL -u=$ADMIN_USER -p=$ADMIN_PWD
+fi
+
+oc create ns openshift-mta
+
+if [ -z "$MTA_OPERATOR_VERSION" ]; then
+    MTA_OPERATOR_VERSION=v7.0.3 # Note: The resources in installmta.yaml should match this version
+    echo "Using the MTA Operator version: $MTA_OPERATOR_VERSION"
+fi
+
+MTA_OPERATOR_NAME=mta-operator."$MTA_OPERATOR_VERSION"
+echo "Using the MTA Operator: $MTA_OPERATOR_NAME"
+
+# Install MTA Operator
+oc apply -f installmta.yaml
+
+oc project openshift-mta
+echo "Installing: MTA operator - please be patient, takes a few minutes"
+sleep 60
+oc delete clusterserviceversion "$MTA_OPERATOR_NAME" -n openshift-mta
+
+sleep 120
+while true; do
+    OPERATOR_INSTALLED=$(oc get clusterserviceversions/"$MTA_OPERATOR_NAME" | grep Succeeded)
+    if [ -n "$OPERATOR_INSTALLED" ]; then
+        echo "Installed: MTA operator "
+        break
+    else
+        echo "Installing: MTA operator "
+        sleep 30
+    fi
+done
+
+# Create Tackle
+oc apply -f tackle.yaml
+
+# Ensure MTA UI Route is available
+echo "Waiting for: MTA UI Route - please be patient, takes a few minutes"
+sleep 420
+while true; do
+    ROUTE=$(oc get route | grep mta-ui)
+    if [ -n "$ROUTE" ]; then
+        echo "MTA Route Available "
+        break
+    else
+        echo "Waiting for: MTA UI Route"
+        sleep 30
+    fi
+done
+
+oc get route
--- mta/v7.x/openshift-local-setup/envup-openshift.sh
+++ mta/v7.x/openshift-local-setup/envup-openshift.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-# Cleanup previous instance
-crc stop
-crc delete -f
-CRC_START_CMD_OUTPUT=$(crc start -o json)
 
-echo "crc start output: $CRC_START_CMD_OUTPUT" 
-
-# Extract variables
-ADMIN_USER=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.adminCredentials.username" | tr -d "\"") 
-ADMIN_PWD=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.adminCredentials.password" | tr -d "\"")
-API_URL=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.url" | tr -d "\"")
-WEB_CONSOLE_URL=$(echo $CRC_START_CMD_OUTPUT | jq ".clusterConfig.webConsoleUrl" | tr -d "\"")
-echo "ADMIN_USER: $ADMIN_USER, ADMIN_PWD: $ADMIN_PWD, API_URL: $API_URL, WEB_CONSOLE_URL: $WEB_CONSOLE_URL"
-
-# Open Web Console
-open $WEB_CONSOLE_URL/k8s/all-namespaces/operators.coreos.com~v1alpha1~ClusterServiceVersion
-
-# Login to the cluster
-oc login -s=$API_URL -u=$ADMIN_USER -p=$ADMIN_PWD
 oc create ns openshift-mta
 
 if [ -z "$MTA_OPERATOR_VERSION" ]; then
@@ -33,10 +15,9 @@ oc apply -f installmta.yaml
 
 oc project openshift-mta
 echo "Installing: MTA operator - please be patient, takes a few minutes"
-sleep 60
+sleep 15
 oc delete clusterserviceversion "$MTA_OPERATOR_NAME" -n openshift-mta
-
-sleep 120
+sleep 30
 while true; do
     OPERATOR_INSTALLED=$(oc get clusterserviceversions/"$MTA_OPERATOR_NAME" | grep Succeeded)
     if [ -n "$OPERATOR_INSTALLED" ]; then
@@ -53,7 +34,7 @@ oc apply -f tackle.yaml
 
 # Ensure MTA UI Route is available
 echo "Waiting for: MTA UI Route - please be patient, takes a few minutes"
-sleep 420
+sleep 30
 while true; do
     ROUTE=$(oc get route | grep mta-ui)
     if [ -n "$ROUTE" ]; then
@@ -66,4 +47,3 @@ while true; do
 done
 
 oc get route
-echo 'MTA UI Credentials: admin Passw0rd!'
